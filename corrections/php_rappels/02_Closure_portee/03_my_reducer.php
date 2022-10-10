f9bc4d8fc5b78f5b6557158d86356169caa59288
FanZy0127 <fanzy28@hotmail.fr>
2022-10-10 16:35:02 +0200

    Correction de la partie sur l'hydratation pour mes alternants bibous d'amour.

--- corrections/php_rappels/02_Closure_portee/03_my_reducer.php
+++ corrections/php_rappels/02_Closure_portee/03_my_reducer.php
@@ -26,3 +26,37 @@ echo my_reduce([1, 2, 3], $sum);
 
 echo PHP_EOL ;
 /* ------- */
+
+$products = [['milk', 3, 3], ['butter', 2.5, 2] , ['eggs', .7, 10]];
+define('TVA', .2);
+
+$hydrate = function () use ($products) {
+    $res = [];
+    // list($name, $price, $quantity) =  ['milk', 3, 3] ; ...
+    foreach ($products as list($name, $price, $quantity)) {
+
+        $res[] = (new class(name: $name, price: $price, quantity: $quantity)
+        {
+            public function __construct(
+                public float $price,
+                public string $name,
+                public float $quantity,
+                public float $tva = TVA
+            ) {}
+        });
+    }
+
+    return $res;
+};
+
+var_dump($hydrate());
+
+echo my_reduce($hydrate(), function($acc, $curr){
+
+    $acc +=  ($curr->price * $curr->quantity) * ($curr->tva + 1);
+
+    return $acc;
+
+}, 0 );
+
+echo PHP_EOL ;
