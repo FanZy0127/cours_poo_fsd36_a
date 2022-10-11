3d5b80c9890de1ec340529650b84071175b7aa94
FanZy0127 <fanzy28@hotmail.fr>
2022-10-11 15:53:35 +0200

    Correction Lamp.

---
+++ corrections/Single_Responsability/1_Lamp/app.php
@@ -0,0 +1,25 @@
+<?php
+
+spl_autoload_register(function ($class) {
+    include __DIR__ . '/' . $class . '.php';
+});
+
+$lamp = new Lamp( new Light );
+
+echo $lamp . PHP_EOL;
+
+$lamp->switch();
+
+echo $lamp . PHP_EOL;
+
+$lamp->switch();
+
+echo $lamp . PHP_EOL;
+
+$lamp->switch();
+
+echo $lamp . PHP_EOL;
+
+$lamp->switch();
+
+echo $lamp . PHP_EOL;
